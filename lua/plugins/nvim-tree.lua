f039a05314c7b90d14f6b43b1adcecee81fb6d92
Pietro Agazzi <pietro.agazzi@prconsulting.eu>
2025-12-18 11:09:38 +0000

    Centralize theme integration for consistent UI styling
    - Move all plugin highlights to Tokyo Night on_highlights hook
    - Add transparent styling for NvimTree, Telescope, WhichKey, and floats
    - Reduce Copilot suggestion opacity using native theme colors
    - Remove duplicate highlight configurations from individual plugins

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -41,13 +41,6 @@ return {
           ignore = false,
         },
       })
-
-      -- Sync NvimTree colors with theme
-      vim.cmd([[
-        highlight NvimTreeNormal guibg=NONE
-        highlight NvimTreeEndOfBuffer guibg=NONE
-        highlight NvimTreeVertSplit guibg=NONE
-      ]])
     end,
     keys = {
        { "<leader>ee", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" }, -- toggle file explorer
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,6 +7,41 @@ return {
         style = "night",
         transparent = true,
         terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        on_highlights = function(hl, c)
+          -- Copilot suggestions with subtle opacity
+          hl.CopilotSuggestion = {
+            fg = c.dark3,
+            italic = true,
+          }
+
+          -- NvimTree transparent integration
+          hl.NvimTreeNormal = { bg = c.none }
+          hl.NvimTreeEndOfBuffer = { bg = c.none }
+          hl.NvimTreeVertSplit = { fg = c.bg_dark, bg = c.none }
+
+          -- Telescope transparent integration
+          hl.TelescopeNormal = { bg = c.none }
+          hl.TelescopeBorder = { fg = c.border_highlight, bg = c.none }
+          hl.TelescopePromptNormal = { bg = c.none }
+          hl.TelescopePromptBorder = { fg = c.border_highlight, bg = c.none }
+          hl.TelescopeResultsNormal = { bg = c.none }
+          hl.TelescopeResultsBorder = { fg = c.border_highlight, bg = c.none }
+          hl.TelescopePreviewNormal = { bg = c.none }
+          hl.TelescopePreviewBorder = { fg = c.border_highlight, bg = c.none }
+
+          -- WhichKey transparent
+          hl.WhichKeyFloat = { bg = c.none }
+
+          -- Floating windows
+          hl.NormalFloat = { bg = c.none }
+          hl.FloatBorder = { fg = c.border_highlight, bg = c.none }
+        end,
       })
       vim.cmd("colorscheme tokyonight")
     end,
